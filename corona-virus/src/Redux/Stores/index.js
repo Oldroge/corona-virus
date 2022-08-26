cc9bfc17134d817ed739827f3ea703f1b5ec2e14
oldroge <rogeriomrjr@gmail.com>
2022-08-26 13:13:21 -0300

    refact: change createStore to configure store and add appyMiddleware

--- corona-virus/src/Redux/Stores/index.js
+++ corona-virus/src/Redux/Stores/index.js
@@ -1,9 +1,14 @@
-import { createStore } from 'redux';
+import { applyMiddleware } from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
+import thunk from 'redux-thunk';
 
 import { composeWithDevTools } from '@redux-devtools/extension';
 
 import RootReducers from '../Reducers';
 
-const store = createStore(RootReducers, composeWithDevTools());
+const store = configureStore(
+  { reducer: RootReducers },
+  composeWithDevTools(applyMiddleware(thunk)),
+);
 
 export default store;
