27875b642369918efe63aabbb01f17e5fae1700c
oldroge <rogeriomrjr@gmail.com>
2022-08-29 18:49:29 -0300

    feat: create a markdown with all countries

--- corona-virus/src/Components/Commons/Search/Search.jsx
+++ corona-virus/src/Components/Commons/Search/Search.jsx
@@ -1,12 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
 import { useLocation } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import ReserchIcon from './images/research.gif';
+import { fetchCovidSummary } from '../../../Redux/Actions/fetchAction';
 
-export default function Search() {
+import './Search.css';
+
+function Search({ fetchSummary, countryData }) {
   const [clicked, setClicked] = useState(false);
 
   const location = useLocation();
+
+  useEffect(() => {
+    fetchSummary();
+  }, []);
 
   const wasClicked = (event) => {
     if (event === 'click') {
@@ -29,14 +38,38 @@ export default function Search() {
           aria-hidden="true"
         />
       ) : null}
+
       {clicked ? (
         <label htmlFor="countries">
           <select name="countries" id="countries">
-            <option label=" " />
-            <option value="br">br</option>
+            <option> </option>
+            {
+              countryData.map((countriesData) => (
+                <option
+                  value={countriesData.CountryCode}
+                  key={countriesData.CountryCode}
+                >
+                  {countriesData.Country}
+                </option>
+              )).sort()
+            }
           </select>
         </label>
       ) : null}
     </div>
   );
 }
+
+const mapDispatchToProps = (dispatch) => ({
+  fetchSummary: () => dispatch(fetchCovidSummary()),
+});
+
+const mapStateToProps = (state) => ({
+  countryData: state.covidDataSummary.countriesList,
+});
+
+Search.propTypes = ({
+  fetchSummary: PropTypes.func,
+}).isRequired;
+
+export default connect(mapStateToProps, mapDispatchToProps)(Search);
