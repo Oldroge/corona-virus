d1ffea36b32d8ffa49570bf944e0be40450e44e9
oldroge <rogeriomrjr@gmail.com>
2022-08-29 14:27:50 -0300

    feat: when clicked on loupe show dropdown

--- corona-virus/src/Components/Header/Header.jsx
+++ corona-virus/src/Components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 
 import './Header.css';
@@ -8,7 +8,19 @@ import CovidIcon from './images/covid_img.png';
 import ReserchIcon from './images/research.gif';
 
 export default function Header() {
+  const [clicked, setClicked] = useState(false);
+
   const location = useLocation();
+
+  const wasClicked = (event) => {
+    if (event === 'click') {
+      setClicked(true);
+    }
+
+    if (clicked === true) {
+      setClicked(false);
+    }
+  };
 
   return (
     <header id="header">
@@ -22,12 +34,24 @@ export default function Header() {
         </div>
         <div>
           {
-            location.pathname === '/details' ? (
+            location.pathname === '/details' && clicked === false ? (
               <img
                 src={ReserchIcon}
                 alt="White background with a animated magnifying glass clickable"
                 className="research-gif"
+                onClick={({ type }) => wasClicked(type)}
+                aria-hidden="true"
               />
+            ) : null
+          }
+          {
+            clicked ? (
+              <label htmlFor="countries">
+                <select name="countries" id="countries">
+                  <option label=" " />
+                  <option value="br">br</option>
+                </select>
+              </label>
             ) : null
           }
         </div>
