67901d3a6220cd22516498a7806de1a98ea9b1dc
oldroge <rogeriomrjr@gmail.com>
2022-08-30 17:07:54 -0300

    fix: pass the new types to dataCovidByCountryReducer file

--- corona-virus/src/Redux/Reducers/dataCovidByCountryReducer.js
+++ corona-virus/src/Redux/Reducers/dataCovidByCountryReducer.js
@@ -1,18 +1,22 @@
-import { GET_COVID_DATA, FAILED_REQUEST, REQUEST_DATA } from '../Actions/fetchCovidAction';
+import {
+  REQUEST_DATA_BY_COUNTRY,
+  GET_COVID_DATA_BY_COUNTRY,
+  FAILED_REQUEST_BY_COUNTRY,
+} from '../Actions/fetchCovidAction';
 
 const INITIAL_STATE = {
-  countriesList: [],
+  byCountry: [],
   error: '',
   loading: false,
 };
 
-function covidDataByCountry(state = INITIAL_STATE, { type, countries, error } = {}) {
+function covidDataByCountry(state = INITIAL_STATE, { type, byCountry, error } = {}) {
   switch (type) {
-    case REQUEST_DATA:
+    case REQUEST_DATA_BY_COUNTRY:
       return { ...state, loading: true };
-    case GET_COVID_DATA:
-      return { ...state, countriesList: countries, loading: false };
-    case FAILED_REQUEST:
+    case GET_COVID_DATA_BY_COUNTRY:
+      return { ...state, byCountry, loading: false };
+    case FAILED_REQUEST_BY_COUNTRY:
       return { ...state, error, loading: false };
     default:
       return state;
