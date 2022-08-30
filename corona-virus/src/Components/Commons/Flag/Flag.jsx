13fb6c31a513aa9116ca5ef67c7481703a38adf1
oldroge <rogeriomrjr@gmail.com>
2022-08-29 22:29:02 -0300

    feat: render flag in Flag component

--- corona-virus/src/Components/Commons/Flag/Flag.jsx
+++ corona-virus/src/Components/Commons/Flag/Flag.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-export default function Flag() {
+import { connect } from 'react-redux';
+
+function Flag({ countriesData }) {
   return (
-    <div>Flag</div>
+    <div>
+      {
+        countriesData ? (
+          <img src={countriesData.flag} alt="" />
+        ) : null
+      }
+    </div>
   );
 }
+
+const mapStateToProps = (state) => ({
+  countriesData: state.dataCountriesReducer.countries,
+});
+
+Flag.propTypes = ({
+  countriesData: PropTypes.array,
+}).isRequired;
+export default connect(mapStateToProps)(Flag);
