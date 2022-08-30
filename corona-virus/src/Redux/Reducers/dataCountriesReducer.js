ac8f26e6cfa973016970499a7bc2c0a7345ec870
oldroge <rogeriomrjr@gmail.com>
2022-08-29 21:04:40 -0300

    feat: create reducer to store countries data

---
+++ corona-virus/src/Redux/Reducers/dataCountriesReducer.js
@@ -0,0 +1,27 @@
+import {
+  REQUEST_DATA,
+  GET_DATA,
+  FAILED_REQUEST,
+} from '../Actions/fetchCountriesAction';
+
+const INITIAL_STATE = {
+  countries: [],
+  erro: '',
+};
+
+function handleCountriesData(state = INITIAL_STATE, { type, countriesData, error } = {}) {
+  switch (type) {
+    case REQUEST_DATA:
+      return { ...state };
+    case GET_DATA:
+      return { ...state, countriesData };
+    case FAILED_REQUEST:
+      return { ...state, error };
+    default:
+      return state;
+  }
+}
+
+export default {
+  handleCountriesData,
+};
