ebea90ebdc89e2f928bd98cc1a73d54d2fce45b4
oldroge <rogeriomrjr@gmail.com>
2022-08-29 21:10:31 -0300

    feat: import dataCountriesReducer in rootreducer

--- corona-virus/src/Redux/Reducers/index.js
+++ corona-virus/src/Redux/Reducers/index.js
@@ -1,7 +1,11 @@
 import { combineReducers } from 'redux';
 
 import covidDataSummary from './dataCovidReducer';
+import dataCountriesReducer from './dataCountriesReducer';
 
-const RootReducers = combineReducers({ covidDataSummary });
+const RootReducers = combineReducers({
+  covidDataSummary,
+  dataCountriesReducer,
+});
 
 export default RootReducers;
