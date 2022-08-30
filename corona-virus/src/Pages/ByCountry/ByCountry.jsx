4e730733ff001c4530745e4017e083863a1ae40d
oldroge <rogeriomrjr@gmail.com>
2022-08-30 10:55:57 -0300

    feat: insert CounterData component in ByCountry page

--- corona-virus/src/Pages/ByCountry/ByCountry.jsx
+++ corona-virus/src/Pages/ByCountry/ByCountry.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 import Header from '../../Components/Header/Header';
 import Flag from '../../Components/Commons/Flag/Flag';
+import CounterData from '../../Components/Commons/CounterData/CounterData';
 
 export default function ByCountry() {
   return (
     <>
       <Header />
       <section>
+        <CounterData />
         <Flag />
       </section>
     </>
