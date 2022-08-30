70a683e53ea0d880a6665274e948a9216dcd7413
oldroge <rogeriomrjr@gmail.com>
2022-08-29 22:55:45 -0300

    feat: insert a an loading key in dataCountriesReducer

--- corona-virus/src/Redux/Reducers/dataCountriesReducer.js
+++ corona-virus/src/Redux/Reducers/dataCountriesReducer.js
@@ -7,16 +7,17 @@ import {
 const INITIAL_STATE = {
   countries: [],
   erro: '',
+  loading: false,
 };
 
 function handleCountriesData(state = INITIAL_STATE, { type, countriesData, error } = {}) {
   switch (type) {
     case REQUEST_DATA:
-      return { ...state };
+      return { ...state, loading: true };
     case GET_DATA:
-      return { ...state, countries: countriesData };
+      return { ...state, countries: countriesData, loading: false };
     case FAILED_REQUEST:
-      return { ...state, error };
+      return { ...state, error, loading: false };
     default:
       return state;
   }
