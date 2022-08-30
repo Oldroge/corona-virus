6cab9ea3f619f156692bbab90a9a0d7a59b003bf
oldroge <rogeriomrjr@gmail.com>
2022-08-30 17:22:38 -0300

    fix: property name in reducer

--- corona-virus/src/Redux/Reducers/dataCovidByCountryReducer.js
+++ corona-virus/src/Redux/Reducers/dataCovidByCountryReducer.js
@@ -5,17 +5,17 @@ import {
 } from '../Actions/fetchCovidAction';
 
 const INITIAL_STATE = {
-  byCountry: [],
+  byCountries: [],
   error: '',
   loading: false,
 };
 
-function covidDataByCountry(state = INITIAL_STATE, { type, byCountry, error } = {}) {
+function covidDataByCountry(state = INITIAL_STATE, { type, byCountries, error } = {}) {
   switch (type) {
     case REQUEST_DATA_BY_COUNTRY:
       return { ...state, loading: true };
     case GET_COVID_DATA_BY_COUNTRY:
-      return { ...state, byCountry, loading: false };
+      return { ...state, byCountries, loading: false };
     case FAILED_REQUEST_BY_COUNTRY:
       return { ...state, error, loading: false };
     default:
