cde99d6e8ecaa37543c034ce7dfe5879c6a072a8
oldroge <rogeriomrjr@gmail.com>
2022-08-30 17:46:14 -0300

    refactor: change state that gets covid datas

--- corona-virus/src/Pages/ByCountry/ByCountry.jsx
+++ corona-virus/src/Pages/ByCountry/ByCountry.jsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropType from 'prop-types';
 import { connect } from 'react-redux';
 
 import Header from '../../Components/Header/Header';
 import Flag from '../../Components/Commons/Flag/Flag';
 import CounterData from '../../Components/Commons/CounterData/CounterData';
+import { fetchCovidByCountry } from '../../Redux/Actions/fetchCovidAction';
 
-function ByCountry({ CovidDatas, CountryDatas, isLoading }) {
+function ByCountry({
+  CovidDatas,
+  CountryDatas,
+  isLoading,
+  fetchByCountry,
+}) {
+  useEffect(() => {
+    fetchByCountry(CountryDatas.alpha2Code);
+  }, [CountryDatas]);
+
   return (
     <>
       <Header />
@@ -22,18 +32,32 @@ function ByCountry({ CovidDatas, CountryDatas, isLoading }) {
               }
               />
               <Flag />
+              <CounterData
+                title="Confirmed"
+                numbers={
+                  CovidDatas[CovidDatas.length - 1]
+                    ? CovidDatas[CovidDatas.length - 1].Confirmed.toLocaleString('pt-BR')
+                    : null
+                }
+              />
 
-              {
-                CovidDatas.map((covidDatas) => (
-                  covidDatas.CountryCode === CountryDatas.alpha2Code ? (
-                    <section>
-                      <CounterData title="Confirmed" numbers={covidDatas.TotalConfirmed} />
-                      <CounterData title="Last Deaths" numbers={covidDatas.NewDeaths} />
-                      <CounterData title="Deaths" numbers={covidDatas.TotalDeaths} />
-                    </section>
-                  ) : null
-                ))
-              }
+              <CounterData
+                title="Actives"
+                numbers={
+                  CovidDatas[CovidDatas.length - 1]
+                    ? CovidDatas[CovidDatas.length - 1].Active.toLocaleString('pt-BR')
+                    : null
+                }
+              />
+
+              <CounterData
+                title="Deaths"
+                numbers={
+                  CovidDatas[CovidDatas.length - 1]
+                    ? CovidDatas[CovidDatas.length - 1].Deaths.toLocaleString('pt-BR')
+                    : null
+                }
+              />
             </>
           )
         }
@@ -46,12 +70,17 @@ ByCountry.propTypes = ({
   CountryDatas: PropType.object,
   CovidDatas: PropType.object,
   isLoading: PropType.bool,
+  fetchByCountry: PropType.func,
 }).isRequired;
 
 const mapStateToProps = (state) => ({
   CountryDatas: state.dataCountriesReducer.countries,
-  CovidDatas: state.covidDataSummary.countriesList,
+  CovidDatas: state.covidDataByCountry.byCountries,
   isLoading: state.dataCountriesReducer.loading,
 });
 
-export default connect(mapStateToProps)(ByCountry);
+const mapDispatchToProps = (dispatch) => ({
+  fetchByCountry: (datas) => dispatch(fetchCovidByCountry(datas)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(ByCountry);
