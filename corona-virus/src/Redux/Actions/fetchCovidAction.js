077591dcb48d014482b1d25c2eb362a1806072ec
oldroge <rogeriomrjr@gmail.com>
2022-08-30 17:06:09 -0300

    fix: fetches in fetchCovidAction was conflited

--- corona-virus/src/Redux/Actions/fetchCovidAction.js
+++ corona-virus/src/Redux/Actions/fetchCovidAction.js
@@ -2,6 +2,10 @@ const REQUEST_DATA = 'REQUEST_DATA';
 const GET_COVID_DATA = 'GET_COVID_DATA';
 const FAILED_REQUEST = 'FAILED_REQUEST';
 
+const REQUEST_DATA_BY_COUNTRY = 'REQUEST_DATA_BY_COUNTRY';
+const GET_COVID_DATA_BY_COUNTRY = 'GET_COVID_DATA_BY_COUNTRY';
+const FAILED_REQUEST_BY_COUNTRY = 'FAILED_REQUEST_BY_COUNTRY';
+
 const requestData = () => ({
   type: REQUEST_DATA,
 });
@@ -27,14 +31,28 @@ const fetchCovidSummary = () => async (dispatch) => {
   }
 };
 
+const requestDataByCountry = () => ({
+  type: REQUEST_DATA_BY_COUNTRY,
+});
+
+const getCovidDataByCountry = (json) => ({
+  type: GET_COVID_DATA_BY_COUNTRY,
+  byCountries: json,
+});
+
+const failedCovidRequestByCountry = (error) => ({
+  type: FAILED_REQUEST_BY_COUNTRY,
+  error: error.message,
+});
+
 const fetchCovidByCountry = (country) => async (dispatch) => {
-  dispatch(requestData());
+  dispatch(requestDataByCountry());
   try {
     const response = await fetch(`https://api.covid19api.com/total/country/${country}`);
     const json = await response.json();
-    dispatch(getCovidData(json));
+    dispatch(getCovidDataByCountry(json));
   } catch (error) {
-    dispatch(failedCovidRequest(error));
+    dispatch(failedCovidRequestByCountry(error));
   }
 };
 
@@ -44,4 +62,7 @@ export {
   GET_COVID_DATA,
   FAILED_REQUEST,
   REQUEST_DATA,
+  REQUEST_DATA_BY_COUNTRY,
+  GET_COVID_DATA_BY_COUNTRY,
+  FAILED_REQUEST_BY_COUNTRY,
 };
